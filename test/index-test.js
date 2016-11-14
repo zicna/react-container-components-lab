edaca3abbc0b49fddb8eb57a67491088dc7879f5
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2016-11-14 19:06:52 +0000

    Renamed SMR tests and add tests to index-test

--- test/index-test.js
+++ test/index-test.js
@@ -1,2 +1,6 @@
 const React = require('react');
 const { shallow } = require('enzyme');
+
+require('./LatestMovieReviewsContainer_test');
+require('./MovieReviews_test');
+require('./SearchableMovieReviewsContainer_test');
