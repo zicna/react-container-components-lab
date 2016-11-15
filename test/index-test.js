ad2cdbac8a5b7a5b74b71b2221f995af3ff8da9c
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2016-11-15 23:51:17 +0000

    test MovieReviews first

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const { shallow } = require('enzyme');
 
-require('./LatestMovieReviewsContainer_test');
 require('./MovieReviews_test');
+require('./LatestMovieReviewsContainer_test');
 require('./SearchableMovieReviewsContainer_test');
