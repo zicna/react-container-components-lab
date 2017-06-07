df495824f0480ae97d496edfad6deea27622764c
Luke Ghenco <lukeghenco@gmail.com>
2017-06-07 06:06:18 -0400

    Remove extra whitespace from index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LatestMovieReviewsContainer from
-  './components/LatestMovieReviewsContainer'
-;
-import SearchableMovieReviewsContainer from
-  './components/SearchableMovieReviewsContainer'
-;
+import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
+import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
 
 ReactDOM.render(
   <div className="app">
