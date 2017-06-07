aa7df49d4e63c823ee160d00b2e24c90c7f3dc75
Gabe Jackson <gj@mail.co.de>
2017-06-07 10:26:34 -0400

    standardize Code _____ Here comments

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -5,4 +5,4 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// CODE LATEST MOVIEREVIEWS CONTAINER HERE
+// Code LatestMovieReviewsContainer Here
