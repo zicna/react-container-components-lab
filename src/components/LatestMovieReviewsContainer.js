61bda3933b759377d166da8e2fc263f8508ccfaa
Luke Ghenco <lukeghenco@gmail.com>
2017-06-07 06:20:46 -0400

    Update LatestMovieReviewsContainer starter code

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,1 +1,8 @@
-// Code LatestMovieReviewsContainer Here
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
+// CODE LATEST MOVIEREVIEWS CONTAINER HERE
