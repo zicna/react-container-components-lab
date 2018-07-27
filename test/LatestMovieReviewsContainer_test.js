855ae94991eebe13c2507e0daf13965c1d164c48
maxwell <github email address>
2018-07-27 09:21:05 -0400

    added fetch test

--- test/LatestMovieReviewsContainer_test.js
+++ test/LatestMovieReviewsContainer_test.js
@@ -5,14 +5,21 @@ import Adapter from 'enzyme-adapter-react-16';
 import LatestMovieReviewsContainer from '../src/components/LatestMovieReviewsContainer';
 import testReviews from './test-reviews';
 
+import { spy, stub, useFakeTimers } from 'sinon'
+
 Enzyme.configure({ adapter: new Adapter() })
 
 const Noop = (props) => { return <p>Noop</p> };
 
 describe('<LatestMovieReviewsContainer />', () => {
   let wrapper;
+  let fetchSpy;
 
   beforeEach(() => {
+    global.fetch = require('node-fetch')
+
+    fetchSpy = spy(global, "fetch")
+
     wrapper = !LatestMovieReviewsContainer.prototype ?
       shallow(<Noop />) : shallow(<LatestMovieReviewsContainer />);
   });
@@ -31,6 +38,11 @@ describe('<LatestMovieReviewsContainer />', () => {
   it('should have top-level element with class "latest-movie-reviews"', () => {
     expect(wrapper.hasClass('latest-movie-reviews')).to.be.true;
   });
+
+  it('should fetch data from the New York Times API', () => {
+    expect(fetchSpy.callCount > 0, "Fetch was not called").to.equal(true);
+    expect(fetchSpy.firstCall.lastArg, "Fetch should have the base URL 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'").to.include('https://api.nytimes.com/svc/movies/v2/reviews/all.json?')
+  })
 
   it('should render reviews after reviews state updated', () => {
     wrapper = !LatestMovieReviewsContainer.prototype ?
--- test/SearchableMovieReviewsContainer_test.js
+++ test/SearchableMovieReviewsContainer_test.js
@@ -5,14 +5,21 @@ import Adapter from 'enzyme-adapter-react-16';
 import SearchableMovieReviewsContainer from '../src/components/SearchableMovieReviewsContainer';
 import testReviews from './test-reviews';
 
+import { spy, stub, useFakeTimers } from 'sinon'
+
 Enzyme.configure({ adapter: new Adapter() })
 
 const Noop = (props) => { return <p>Noop</p> };
 
 describe('<SearchableMovieReviewsContainer />', () => {
   let wrapper;
+  let fetchSpy;
 
   beforeEach(() => {
+    global.fetch = require('node-fetch')
+
+    fetchSpy = spy(global, "fetch")
+
     wrapper = !SearchableMovieReviewsContainer.prototype ?
       shallow(<Noop />) : shallow(<SearchableMovieReviewsContainer />);
   });
@@ -31,6 +38,11 @@ describe('<SearchableMovieReviewsContainer />', () => {
   it('should have top-level element with class "searchable-movie-reviews"', () => {
     expect(wrapper.hasClass('searchable-movie-reviews'), 'Missing top-level element with class "searchable-movie-reviews"').to.be.true;
   });
+
+  it('should fetch data from the New York Times API', () => {
+    expect(fetchSpy.callCount > 0, "Fetch was not called").to.equal(true);
+    expect(fetchSpy.firstCall.lastArg, "Fetch should have the base URL 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'").to.include('https://api.nytimes.com/svc/movies/v2/reviews/all.json?')
+  })
 
   it('should render reviews after reviews state updated', () => {
     wrapper = !SearchableMovieReviewsContainer.prototype ?
