5ae441a4d298cc9d01b607eb766cc84e04799a94
maxwellbenton <maxwellbenton@gmail.com>
2019-01-28 15:19:53 -0500

    updates solution to handle length error

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,16 +4,18 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-                 + `api-key=${NYT_API_KEY}&query=`;
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`;
 
 class SearchableMovieReviewsContainer extends Component {
-
   state = {
     searchTerm: '',
     reviews: []
+  };
 
-  handleSearchInputChange = event => this.setState({ searchTerm: event.target.value });
+  handleSearchInputChange = event =>
+    this.setState({ searchTerm: event.target.value });
 
   handleSubmit = event => {
     event.preventDefault();
@@ -21,26 +23,27 @@ class SearchableMovieReviewsContainer extends Component {
     fetch(BASE_URL.concat(this.state.searchTerm))
       .then(res => res.json())
       .then(res => this.setState({ reviews: res.results }));
-  }
+  };
 
   render() {
     return (
       <div className="searchable-movie-reviews">
-        <form onSubmit={ this.handleSubmit }>
-          <label htmlFor='search-input'>Search Movie Reviews</label>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search-input">Search Movie Reviews</label>
           <input
-            id='search-input'
+            id="search-input"
             type="text"
             style={{ width: 300 }}
-            onChange={ this.handleSearchInputChange } />
+            onChange={this.handleSearchInputChange}
+          />
           <button type="submit">Submit</button>
         </form>
-        { this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2> }
-        <MovieReviews reviews={ this.state.reviews } />
+        {typeof this.state.reviews === 'object' &&
+          this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
+        <MovieReviews reviews={this.state.reviews} />
       </div>
     );
   }
 }
-
 
 export default SearchableMovieReviewsContainer;
